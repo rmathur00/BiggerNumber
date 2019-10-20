c541126aa99fdcd7ca6bbf5a80069546a33e66e1
Ritik Mathur <1824433@brunel.ac.uk>
2019-10-21 00:47:15 +0100

    Level 2 commit

--- app/src/main/java/com/example/biggernumber/MainActivity.java
+++ app/src/main/java/com/example/biggernumber/MainActivity.java
@@ -5,12 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     Button b1,b2;
+    TextView msg;
+    int number1, number2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,20 +22,42 @@ public class MainActivity extends AppCompatActivity {
 
         b1 = findViewById(R.id.Button1);
         b2 = findViewById(R.id.Button2);
+        msg = findViewById(R.id.message);
 
         randonNumberGenerator();
 
     }
 
-    private void randonNumberGenerator(){
+    public void randonNumberGenerator(){
 
         Random num = new Random();
-        int number1 = num.nextInt(11-1)+1;
-        int number2 = num.nextInt(11-1)+1;
+        number1 = num.nextInt(11-1)+1;
 
-        b1.setText(Integer.toString(number1));
-        b2.setText(Integer.toString(number2));
+        while (true){
+            number2 = num.nextInt(11-1)+1;
 
+            if(number1!= number2){
+                b1.setText(Integer.toString(number1));
+                b2.setText(Integer.toString(number2));
+                break;
+            }
+        }
     }
 
+    public void leftButton(View view){
+        if(number1>number2){
+            msg.setText("CONGRATULATIONS ^_^");
+        }else{
+            msg.setText("Wrong: "+ number2 + ">" + number1 );
+        }
+        randonNumberGenerator();
+    }
+    public void rightButton(View view){
+        if(number2>number1){
+            msg.setText("CONGRATULATIONS ^_^");
+        }else{
+            msg.setText("Wrong: "+ number1 + ">" + number2 );
+        }
+        randonNumberGenerator();
+    }
 }
